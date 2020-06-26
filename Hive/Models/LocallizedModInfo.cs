5441830840a78982f1ff6d3baac9641c312de2be
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-06-25 23:24:20 -0500

    Added basic model proposal

---
+++ Hive/Models/LocallizedModInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hive.Models
+{
+    public class LocalizedModInfo
+    {
+        public CultureInfo Language { get; } = null!;
+
+        public string Name { get; } = null!;
+
+        public string Description { get; } = null!;
+
+        public string? Changelog { get; }
+
+        public string? Credits { get; }
+
+        #region DB Schema stuff
+        // this would be a foreign key back to the Mod object this is associated with
+        public Guid ModVersionGuid { get; }
+        #endregion
+    }
+}
---
+++ Hive/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hive.Models
+{
+    // User would ideally come from the auth server, and be a thin proxy to the appropriate RPC calls
+    public class User
+    {
+        // this should have at least:
+        // - name
+        // - unique id (might be name?)
+        // - references to localization objects ala LocalizedModInfo with:
+        //   - bio
+        // - links to various info about the user
+        // - a prfile pic
+        // - an extra data object like Mod.AdditionalData
+    }
+}
