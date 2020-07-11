5fa0d28cc5f13fdd60bcd46cb9f28fd43e8ea14d
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-07-11 02:44:50 -0500

    Added test code for EF in WFC
    FIXME: The Dependencies list is not cottectly deserialized

--- Hive/Controllers/WeatherForecastController.cs
+++ Hive/Controllers/WeatherForecastController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hive.Models;
 using Hive.Permissions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hive.Controllers
 {
@@ -16,13 +18,15 @@ namespace Hive.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+        private readonly ModsContext context;
         private readonly Serilog.ILogger log;
         private readonly PermissionsManager<PermissionContext> permissions;
 
-        public WeatherForecastController(Serilog.ILogger log, PermissionsManager<PermissionContext> perms)
+        public WeatherForecastController(Serilog.ILogger log, PermissionsManager<PermissionContext> perms, ModsContext ctx)
         {
             this.log = log.ForContext<WeatherForecastController>();
             permissions = perms;
+            context = ctx;
         }
 
         private static PermissionActionParseState action;
@@ -34,6 +38,13 @@ namespace Hive.Controllers
 
             if (!permissions.CanDo("hive.weather.get", new PermissionContext(), ref action))
                 throw new InvalidOperationException();
+
+            var mod = context.Mods
+                .Include(m => m.Localizations)
+                .AsEnumerable()
+                .Where(m => m.Localizations.Any(l => l.Language.Name == "en-US"))
+                .Select(m => new { Mod = m, Info = m.Localizations.First(l => l.Language.Name == "en-US") })
+                .ToList();
 
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
