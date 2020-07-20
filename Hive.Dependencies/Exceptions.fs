b9420a70ba8ed1578288b26d83341441b53f4d02
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-07-19 20:15:27 -0500

    Added Exceptions and resolver signature

---
+++ Hive.Dependencies/Exceptions.fs
@@ -0,0 +1,8 @@
+﻿namespace Hive.Dependencies
+
+type VersionNotFoundException<'ModRef>(ref: 'ModRef) =
+    inherit exn("A mod could not be found matching reference")
+    member public _.ModReference = ref
+    override _.Message = base.Message + " " + ref.ToString()
+
+
---
+++ Hive.Dependencies/Resolver.fsi
@@ -0,0 +1,6 @@
+﻿namespace Hive.Dependencies
+
+open System.Threading.Tasks
+
+module Resolver =
+    val Resolve : accessor: IValueAccessor<'Mod, 'ModRef, 'Version, 'VerRange> -> mods: 'Mod seq -> Task<'Mod seq>
