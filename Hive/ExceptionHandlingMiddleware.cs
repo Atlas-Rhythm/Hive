6122e925f5466ca0b0f8913aedd37c55d44b9786
Adam ? <adamznow@gmail.com>
2020-07-22 01:10:47 -0700

    Add exception handling middleware and ApiException
    TODO: Write docs on ApiException

---
+++ Hive/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Hive.Plugin;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Hive
+{
+    internal struct JsonApiException
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionHandlingMiddleware
+    {
+        private static readonly JsonSerializerOptions serializerOptions = new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault
+        };
+
+        private readonly RequestDelegate _next;
+        private readonly Serilog.ILogger logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, Serilog.ILogger log)
+        {
+            _next = next;
+            logger = (log ?? throw new ArgumentException(Resource.ArgumentNullException_ExceptionHandlingMiddleware, nameof(log))).ForContext<ExceptionHandlingMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            string? message = null;
+            try
+            {
+                await _next.Invoke(httpContext);
+            }
+            catch (ApiException ex)
+            {
+                message = ex.Message;
+                logger.Error(ex, "Handling API Exception");
+                httpContext.Response.StatusCode = ex.StatusCode;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                logger.Error(ex, "Internal server exception");
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
+            // TODO: This should be changed for GraphQL endpoints probably
+            if (!string.IsNullOrEmpty(message) && !httpContext.Response.HasStarted)
+            {
+                httpContext.Response.ContentType = "application/json";
+                var json = JsonSerializer.Serialize(new JsonApiException { StatusCode = httpContext.Response.StatusCode, Message = message }, serializerOptions);
+                await httpContext.Response.WriteAsync(json);
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionHandlingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}
---
+++ Hive/Plugin/ApiException.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Hive.Plugin
+{
+    public class ApiException : Exception
+    {
+        public int StatusCode { get; }
+
+        public new string? Message { get; }
+
+        public ApiException(HttpStatusCode statusCode, string? message = null)
+        {
+            StatusCode = (int)statusCode;
+            Message = message ?? StatusCodeDefaultMessage(statusCode);
+        }
+
+        private static string? StatusCodeDefaultMessage(HttpStatusCode code)
+        {
+            switch (code)
+            {
+                case HttpStatusCode.NotFound:
+                    return "Not found";
+
+                case HttpStatusCode.BadRequest:
+                    return "Bad request";
+
+                case HttpStatusCode.Forbidden:
+                    return "Forbidden";
+
+                case HttpStatusCode.InternalServerError:
+                    return "Internal server error";
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
