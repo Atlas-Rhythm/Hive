16ee6793dc6f2a5318f1423048ce12dcdc392940
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-08-18 21:59:47 -0500

    Added support to the fuzzing program for fuzzing range operations

--- Hive.Versioning.Fuzz/Program.cs
+++ Hive.Versioning.Fuzz/Program.cs
@@ -1,20 +1,50 @@
 ﻿using Hive.Versioning.Tests;
 using SharpFuzz;
 using System;
+using System.Linq;
 
 namespace Hive.Versioning.Fuzz
 {
     class Program
     {
-        static void Main()
+        static int Main(string[] args)
         {
-            var fix = new VersionTestFixture();
-            Fuzzer.Run(vertext =>
+            var type = args.Length > 0 ? args[0] : "range";
+
+            if (type == "version")
             {
-                vertext = vertext.Trim();
-                var success = Version.TryParse(vertext, out var ver);
-                fix.Validate(success, vertext, ver);
-            });
+                var fix = new VersionTestFixture();
+                Fuzzer.Run(vertext =>
+                {
+                    vertext = vertext.Trim();
+                    var success = Version.TryParse(vertext, out var ver);
+                    fix.Validate(success, vertext, ver);
+                });
+            }
+            else if (type == "range")
+            {
+                Fuzzer.Run(rangetext =>
+                {
+                    rangetext = rangetext.Trim();
+                    if (VersionRange.TryParse(rangetext, out var range))
+                    {
+                        if (args.Contains("disj"))
+                        {
+                            range |= range;
+                            range |= range;
+                            range |= range;
+                            range |= range;
+                        }
+                        _ = range.ToString();
+                    }
+                });
+            }
+            else
+            {
+                return 1;
+            }
+
+            return 0;
         }
     }
 }
