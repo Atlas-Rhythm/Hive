6bdfa006921d776b2171b6fa70b1fd231d563d2c
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-09-11 15:45:59 -0500

    Added attributes and a small handful of checks for them

--- Hive.Plugins/AggregatedMethodGenerator.cs
+++ Hive.Plugins/AggregatedMethodGenerator.cs
@@ -10,10 +10,37 @@ namespace Hive.Plugins
     {
         public static Delegate Generate(Type iface, MethodInfo toAggregate, Type delegateType)
         {
+            var stopIfReturnsAttr = toAggregate.GetCustomAttribute<StopIfReturnsAttribute>();
+            var stopIfReturnsNullAttr = toAggregate.GetCustomAttribute<StopIfReturnsNullAttribute>();
+            var returnLastAttribute = toAggregate.GetCustomAttribute<ReturnLastAttribute>();
+
+            if (stopIfReturnsAttr != null && stopIfReturnsNullAttr != null)
+                throw new InvalidOperationException($"Method {toAggregate} cannot have both {nameof(StopIfReturnsAttribute)} and {nameof(StopIfReturnsNullAttribute)}");
+
+            if (stopIfReturnsAttr != null && !CheckAttribute(toAggregate.ReturnParameter, stopIfReturnsAttr))
+                throw new InvalidOperationException($"Method {toAggregate} must return {typeof(bool)} to use {nameof(StopIfReturnsAttribute)}");
+
+            if (stopIfReturnsNullAttr != null && !CheckAttribute(toAggregate.ReturnParameter, stopIfReturnsNullAttr))
+                throw new InvalidOperationException($"Method {toAggregate} must return a nullable type to use {nameof(StopIfReturnsNullAttribute)}");
+
             var expr = Expression.Lambda(delegateType,
                 Expression.Constant(null));
 
             return expr.Compile();
+        }
+
+        private static bool CheckAttribute(ParameterInfo param, Attribute attr)
+        {
+            if (!CheckAttributeTarget(param, attr)) return false;
+            if (attr is IRequiresType reqTy) return reqTy.CheckType(param.ParameterType);
+            return true;
+        }
+
+        private static bool CheckAttributeTarget(ParameterInfo param, Attribute attr)
+        {
+            if (param.IsRetval) return attr is ITargetsReturn;
+            if (param.IsOut) return attr is ITargetsOutParam;
+            return attr is ITargetsInParam;
         }
     }
 }
---
+++ Hive.Plugins/_Attributes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hive.Plugins
+{
+    internal interface ITargetsInParam { }
+    internal interface ITargetsOutParam { }
+    internal interface ITargetsReturn { }
+
+    internal interface IRequiresType
+    {
+        bool CheckType(Type type);
+    }
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Parameter, AllowMultiple = false)]
+    public sealed class StopIfReturnsAttribute : Attribute, ITargetsOutParam, ITargetsReturn, IRequiresType
+    {
+        public bool ReturnValue { get; }
+        public StopIfReturnsAttribute(bool returnValue)
+            => ReturnValue = returnValue;
+
+        bool IRequiresType.CheckType(Type type)
+            => type == typeof(bool);
+    }
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Parameter, AllowMultiple = false)]
+    public sealed class StopIfReturnsNullAttribute : Attribute, ITargetsOutParam, ITargetsReturn, IRequiresType
+    {
+        bool IRequiresType.CheckType(Type type)
+            => !type.IsValueType || (type.IsConstructedGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>));
+    }
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Parameter, AllowMultiple = false)]
+    public sealed class ReturnLastAttribute : Attribute, ITargetsOutParam, ITargetsReturn
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue, AllowMultiple = false)]
+    public sealed class AggregateWithAttribute : Attribute, ITargetsOutParam, ITargetsReturn
+    {
+        public Type TypeWithAggregator { get; }
+        public string AggregatorName { get; }
+        public AggregateWithAttribute(Type targetType, string targetName)
+            => (TypeWithAggregator, AggregatorName) = (targetType, targetName);
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+    public sealed class TakesReturnValueAttribute : Attribute, ITargetsInParam
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+    public sealed class TakesOutParamValueAttribute : Attribute, ITargetsOutParam
+    {
+        public int ParameterIndex { get; }
+        public TakesOutParamValueAttribute(int paramIndex)
+            => ParameterIndex = paramIndex;
+    }
+
+}
