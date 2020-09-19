c0662e1eaa6c9954d38d3341bf45f6d27fa779ff
Caeden117 <caeden.s@outlook.com>
2020-09-19 01:05:34 -0700

    Add methods for GameVersions plugin interface

--- Hive/Controllers/GameVersionsController.cs
+++ Hive/Controllers/GameVersionsController.cs
@@ -18,7 +18,22 @@ namespace Hive.Controllers
     [Aggregable]
     public interface IGameVersionsPlugin
     {
-        // TODO: Add methods for stuff that should be pluginable, or remove if there isn't any
+        /// <summary>
+        /// Returns true if the sepcified user has access to view the list of all game versions. False otherwise.
+        /// A false return will cause the endpoint to return a Forbid before executing the rest of the endpoint.
+        /// <para>It is recommended to use <see cref="GetGameVersionsFilter(User?, IEnumerable{GameVersion})"/> for filtering out game versions.</para>
+        /// <para>Hive default is to return true.</para>
+        /// </summary>
+        /// <param name="user">User in context</param>
+        public bool GetGameVersionsAdditionalChecks(User? user) => true;
+
+        /// <summary>
+        /// Returns a filtered enumerable of <see cref="GameVersion"/>.
+        /// <para>Hive default is to return input game versions.</para>
+        /// </summary>
+        /// <param name="user">User to filter on</param>
+        /// <param name="versions">Input versions to filter</param>
+        public IEnumerable<GameVersion> GetGameVersionsFilter(User? user, [TakesReturnValue] IEnumerable<GameVersion> versions) => versions;
     }
 
     internal class HiveGameVersionsControllerPlugin : IGameVersionsPlugin { }
@@ -58,14 +73,25 @@ namespace Hive.Controllers
             if (!permissions.CanDo(ActionName, new PermissionContext { User = user }, ref versionsParseState))
                 return Forbid();
 
+            // Combine plugins
+            log.Debug("Combining plugins...");
+            var combined = plugin.Instance;
+            log.Debug("Perform additional checks for GetGameVersions...");
+            // If the plugins say the user cannot access the list of game versions, then we forbid.
+            if (!combined.GetGameVersionsAdditionalChecks(user))
+                return Forbid();
+
             // Grab our list of game versions
             var versions = context.GameVersions.ToList();
             log.Debug("Filtering versions from all {0} versions...", versions.Count);
-            // First, we perform a permission check on each game version, in case we need to filter any specific ones (Beta game versions, perhaps?)
+            // First, we perform a permission check on each game version, in case we need to filter any specific ones
+            // (Use additionalData to flag beta game versions, perhaps? Could be a plugin.)
             var filteredVersions = versions.Where(v => permissions.CanDo(ActionName, new PermissionContext { GameVersion = v, User = user }, ref versionsParseState));
             log.Debug("Remaining versions after permissions check: {0}", filteredVersions.Count());
-            // TODO: Add plugin method for further filtering specific game versions
-            log.Debug("Remaining versions to return: {0}", filteredVersions.Count());
+            // Then we filter this even further by passing it through all of our Hive plugins.
+            filteredVersions = combined.GetGameVersionsFilter(user, filteredVersions);
+            // This final filtered list of versions is what we'll return back to the user.
+            log.Debug("Remaining versions after plugin filters: {0}", filteredVersions.Count());
             return Ok(filteredVersions.ToList());
         }
     }
