6cf90575b19c559f43fec7215a39eb1f72c3f465
Caeden117 <caeden.s@outlook.com>
2020-10-17 22:10:20 -0700

    Add serialized mod and localized mod info models

---
+++ Hive/Models/Serialized/SerializedMod.cs
@@ -0,0 +1,41 @@
+﻿using Hive.Versioning;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Hive.Models.Serialized
+{
+    /// <summary>
+    /// A serializer-friendly version of a <see cref="Mod"/>.
+    /// </summary>
+    public class SerializedMod
+    {
+        public string Name { get; init; } = null!;
+
+        public Version Version { get; init; } = null!;
+
+        public string UpdatedAt { get; init; } = null!;
+
+        public string EditedAt { get; init; } = null!;
+
+        public string UploaderUsername { get; init; } = null!;
+
+        public string ChannelName { get; init; } = null!;
+
+        public string DownloadLink { get; init; } = null!;
+
+        public IList<string> Authors { get; } = new List<string>();
+
+        public IList<string> Contributors { get; } = new List<string>();
+
+        public IList<string> SupportedGameVersions { get; } = new List<string>();
+
+        public IList<(string, string)> Links { get; } = new List<(string, string)>();
+
+        public IList<ModReference> Dependencies { get; } = new List<ModReference>();
+
+        public IList<ModReference> ConflictsWith { get; } = new List<ModReference>();
+
+        // all AdditionalData fields are public, yet readonly.
+        public JsonElement AdditionalData { get; init; }
+    }
+}
