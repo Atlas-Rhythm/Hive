6f7ea804f95fdf2d7544bd1a5675f7ec40b1bfa9
Adam ? <adamznow@gmail.com>
2020-10-24 14:13:15 -0700

    Add DbHelper, DIHelper partial restructure, ParialContext, TestDbWrapper
    Workflow is that a unique DB is created per test run, in the constructor of the test. This causes a bit of a slowdown, for tests, but overall avoids race conditions on the DB itself.

---
+++ Hive.Tests/DbHelper.cs
@@ -0,0 +1,47 @@
+﻿using Hive.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hive.Tests
+{
+    internal static class DbHelper
+    {
+        /// <summary>
+        /// Copies data from <see cref="PartialContext"/> to <see cref="HiveContext"/>
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="dbContext"></param>
+        internal static void CopyData(PartialContext context, HiveContext dbContext)
+        {
+            if (context.Channels is not null)
+                dbContext.Channels.AddRange(context.Channels);
+            if (context.GameVersions is not null)
+                dbContext.GameVersions.AddRange(context.GameVersions);
+            if (context.ModLocalizations is not null)
+                dbContext.ModLocalizations.AddRange(context.ModLocalizations);
+            if (context.Mods is not null)
+                dbContext.Mods.AddRange(context.Mods);
+            dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Sets up the Database. Should only be called once per test (in the test constructor).
+        /// </summary>
+        /// <param name="options"></param>
+        internal static void SetupDb(DbContextOptions<HiveContext> options, PartialContext? initialData = null)
+        {
+            using var dbContext = new HiveContext(options);
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+            if (initialData is not null)
+            {
+                CopyData(initialData, dbContext);
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Hive.Tests/TestDbWrapper.cs
@@ -0,0 +1,44 @@
+﻿using Hive.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hive.Tests
+{
+    /// <summary>
+    /// An extendable class that indicates a test is for
+    /// </summary>
+    public class TestDbWrapper
+    {
+        /// <summary>
+        /// The <see cref="DbContextOptions{TContext}"/> that have been initialized to point to a test DB.
+        /// </summary>
+        protected DbContextOptions<HiveContext> Options { get; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "<Pending>")]
+        public TestDbWrapper(string testId, PartialContext? context = null)
+        {
+            // DB name for the test
+            var dbName = "test_" + testId;
+            // Get connection string
+            var config = new ConfigurationBuilder().AddUserSecrets(typeof(Program).Assembly).Build();
+            var connectionString = config.GetConnectionString("Test");
+            // Create DB with connection string
+            //var outterConnection = new NpgsqlConnection(connectionString);
+            //outterConnection.Open();
+            //var cmd = new NpgsqlCommand($"DROP DATABASE IF EXISTS {dbName}; CREATE DATABASE {dbName};", outterConnection);
+            //cmd.ExecuteNonQuery();
+            //outterConnection.Close();
+            // Create Options
+            Options = new DbContextOptionsBuilder<HiveContext>().UseNpgsql(connectionString + "Database='" + dbName + "';", o => o.UseNodaTime()).Options;
+            // Setup DB using context
+            DbHelper.SetupDb(Options, context);
+        }
+    }
+}
