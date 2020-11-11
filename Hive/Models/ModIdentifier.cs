d9b22f6529bfcbff068dc83b172ceea6abe00177
Caeden117 <caeden.s@outlook.com>
2020-11-10 20:03:55 -0800

    Add ToString to ModIdentifier

--- Hive/Models/ModIdentifier.cs
+++ Hive/Models/ModIdentifier.cs
@@ -4,5 +4,7 @@
     {
         public string ID { get; init; } = null!;
         public string Version { get; init; } = null!;
+
+        public override string ToString() => $"{ID}@{Version}";
     }
 }
