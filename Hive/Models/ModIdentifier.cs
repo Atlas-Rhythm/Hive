6b74016fa58299f6a0824a5ad05da7fdfdc26732
Caeden117 <caeden.s@outlook.com>
2020-11-21 19:23:24 -0800

    Revert "Add ToString to ModIdentifier"
    This reverts commit d9b22f6529bfcbff068dc83b172ceea6abe00177.

--- Hive/Models/ModIdentifier.cs
+++ Hive/Models/ModIdentifier.cs
@@ -4,7 +4,5 @@
     {
         public string ID { get; init; } = null!;
         public string Version { get; init; } = null!;
-
-        public override string ToString() => $"{ID}@{Version}";
     }
 }
