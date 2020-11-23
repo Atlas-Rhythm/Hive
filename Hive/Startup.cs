df2183392aef8e0d54f42bd9c0c57e8cb269a90b
Caeden117 <caeden.s@outlook.com>
2020-11-23 15:12:39 -0800

    Span -> sp

--- Hive/Startup.cs
+++ Hive/Startup.cs
@@ -43,9 +43,9 @@ namespace Hive
                 .AddSingleton(sp =>
                     new PermissionsManager<PermissionContext>(sp.GetRequiredService<IRuleProvider>(), sp.GetService<Permissions.Logging.ILogger>(), "."))
                 .AddSingleton<IChannelsControllerPlugin>(sp => new HiveChannelsControllerPlugin())
-                .AddSingleton<IGameVersionsPlugin>(Span => new HiveGameVersionsControllerPlugin())
-                .AddSingleton<IModsPlugin>(Span => new HiveModsControllerPlugin())
-                .AddSingleton<IResolveDependenciesPlugin>(Span => new HiveResolveDependenciesControllerPlugin())
+                .AddSingleton<IGameVersionsPlugin>(sp => new HiveGameVersionsControllerPlugin())
+                .AddSingleton<IModsPlugin>(sp => new HiveModsControllerPlugin())
+                .AddSingleton<IResolveDependenciesPlugin>(sp => new HiveResolveDependenciesControllerPlugin())
                 //.AddSingleton<IProxyAuthenticationService>(sp => new VaulthAuthenticationService(sp.GetService<Serilog.ILogger>(), sp.GetService<IConfiguration>()));
                 .AddSingleton<IProxyAuthenticationService>(sp => new MockAuthenticationService());
 
