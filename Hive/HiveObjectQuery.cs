1338f232b16469e54af5e51859107705d6af07bd
Auros Nexus <aurosnex@gmail.com>
2020-12-17 18:31:00 -0500

    Added Conversions

--- Hive/HiveObjectQuery.cs
+++ Hive/HiveObjectQuery.cs
@@ -1,4 +1,22 @@
-﻿namespace Hive
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace Hive
 {
-    public record HiveObjectQuery<T>(T? Value, string? Message, int StatusCode);
+    public record HiveObjectQuery<T>(T? Value, string? Message, int StatusCode)
+    {
+        public ActionResult<T> Convert()
+        {
+            return StatusCode switch
+            {
+                StatusCodes.Status200OK => new OkObjectResult(Value),
+                StatusCodes.Status204NoContent => new NoContentResult(),
+                StatusCodes.Status400BadRequest => new BadRequestObjectResult(Message),
+                StatusCodes.Status401Unauthorized => new UnauthorizedObjectResult(Message),
+                StatusCodes.Status403Forbidden => new ForbidResult(),
+                StatusCodes.Status404NotFound => new NotFoundResult(),
+                _ => new ObjectResult(Message) { StatusCode = StatusCode },
+            };
+        }
+    }
 }
