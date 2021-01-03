0fce2d82034f1185b3b0c40e8b1f3ff01a31fa78
Caeden117 <caeden.s@outlook.com>
2021-01-03 00:01:09 -0800

    First iteration of file system rule provider

--- Hive/Permissions/ConfigRuleProvider.cs
+++ Hive/Permissions/ConfigRuleProvider.cs
@@ -1,34 +1,103 @@
 ﻿using Hive.Utilities;
 using NodaTime;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 
 namespace Hive.Permissions
 {
     /// <summary>
-    ///
+    /// The default rule provider for Hive, which utilizes the file system.
     /// </summary>
     public class ConfigRuleProvider : IRuleProvider
     {
-        // TODO: implement
+        private readonly string ruleDirectory = Path.Combine(Environment.CurrentDirectory, "Rules");
+        private readonly string defaultRuleDefinition = "next(true)";
+        private readonly StringView splitToken = "*";
+
+        // Rule name to information corresponding to the particular rule.
+        private readonly Dictionary<string, (FileInfo, Rule)> cachedFileInfos = new();
 
         /// <summary>
-        ///
+        /// Construct a rule provider via DI.
         /// </summary>
         public ConfigRuleProvider()
         {
+            // TODO: Read base directory (and default rule value) from config
         }
 
         /// <inheritdoc/>
-        public bool HasRuleChangedSince(StringView name, Instant time) => false;
+        public bool HasRuleChangedSince(StringView name, Instant time)
+        {
+            var fileInfo = TryGetRule(name.ToString(), out var newlyCreatedRule).Item1;
+            return fileInfo.LastWriteTimeUtc != time.ToDateTimeUtc() || newlyCreatedRule;
+        }
 
         /// <inheritdoc/>
-        public bool HasRuleChangedSince(Rule rule, Instant time) => false;
+        public bool HasRuleChangedSince(Rule rule, Instant time)
+        {
+            if (rule is null) throw new ArgumentNullException(nameof(rule));
+
+            var fileInfo = TryGetRule(rule.Name, out var newlyCreatedRule).Item1;
+            return fileInfo.LastWriteTimeUtc != time.ToDateTimeUtc() || newlyCreatedRule;
+        }
 
         /// <inheritdoc/>
         public bool TryGetRule(StringView name, [MaybeNullWhen(false)] out Rule gotten)
         {
-            gotten = new Rule(name.ToString(), "next(true)");
+            gotten = TryGetRule(name.ToString(), out _).Item2;
             return true;
+        }
+
+        // Helper function that obtains cached information about a rule. If none exists, it goes to the file system.
+        private (FileInfo, Rule) TryGetRule(string name, out bool newlyCreated)
+        {
+            if (cachedFileInfos.TryGetValue(name, out var fileInfo))
+            {
+                newlyCreated = false;
+                return fileInfo;
+            }
+            else
+            {
+                var fromFileSystem = GetOrCreateFromFileSystem(name, GetRuleLocation(name), out newlyCreated);
+                cachedFileInfos.Add(name, fromFileSystem);
+                return fromFileSystem;
+            }
+        }
+
+        // Helper function that reads information about a rule from the file system.
+        // If the rule does not exist in the file system, we write a new rule with the default definition.
+        private (FileInfo, Rule) GetOrCreateFromFileSystem(string ruleName, string filePath, out bool newlyCreated)
+        {
+            newlyCreated = false;
+            var ruleDefinition = defaultRuleDefinition;
+
+            if (File.Exists(filePath))
+            {
+                ruleDefinition = File.ReadAllText(filePath);
+            }
+            else
+            {
+                newlyCreated = true;
+
+                using var writer = File.CreateText(filePath);
+                writer.WriteLine(ruleDefinition);
+            }
+
+            var fileInfo = new FileInfo(filePath);
+            var rule = new Rule(ruleName, ruleDefinition);
+
+            return (fileInfo, rule);
+        }
+
+        // Helper function that returns the file system location for a rule
+        private string GetRuleLocation(StringView ruleName)
+        {
+            var parts = ruleName.Split(splitToken, ignoreEmpty: false);
+            var localRuleDirectory = string.Join(@"\", parts);
+
+            return Path.Combine(ruleDirectory, localRuleDirectory, ".rule");
         }
     }
 }
