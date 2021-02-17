8b18c1833fd450f7f498bf1fe11860f04d18efd6
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-02-16 22:45:00 -0600

    General warning cleanup

--- Hive.Plugins.Tests/TestPluginLoading.cs
+++ Hive.Plugins.Tests/TestPluginLoading.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Hive.Plugins.Loading;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
