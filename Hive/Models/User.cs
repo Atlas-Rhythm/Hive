d59f7236a56ef49efb871b03d01ae5d52e42ccc9
Adam ? <adamznow@gmail.com>
2021-05-29 20:45:58 -0700

    Fixed usings

--- Hive/Models/User.cs
+++ Hive/Models/User.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
-using Microsoft.EntityFrameworkCore;
 
 namespace Hive.Models
 {
