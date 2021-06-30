d360c34426e3361c5bc3d96742577e2cd76893da
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-06-29 22:11:09 -0500

    Add plugin template

---
+++ Hive.Plugin.Template/PluginTemplate/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Hive.Plugins;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PluginTemplate
+{
+    [PluginStartup]
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+            => Configuration = config;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // TODO: Register your services to the IServiceCollection
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // TODO: Configure the application
+        }
+    }
+}
