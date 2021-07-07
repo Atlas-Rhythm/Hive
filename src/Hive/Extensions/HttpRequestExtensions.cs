32280319b94e353d5f0253336989e678a3fce9dd
Caeden Statia <caeden.s@outlook.com>
2021-07-07 13:14:15 -0700

    complete XML docs that I forgot to complete

--- src/Hive/Extensions/HttpRequestExtensions.cs
+++ src/Hive/Extensions/HttpRequestExtensions.cs
@@ -8,18 +8,18 @@ using Microsoft.AspNetCore.Http;
 namespace Hive.Extensions
 {
     /// <summary>
-    /// Extensions for an <see cref="HttpContext"/>.
+    /// Extensions for an <see cref="HttpRequest"/>.
     /// </summary>
     public static class HttpRequestExtensions
     {
         private static readonly Dictionary<HttpRequest, User?> cachedUsers = new();
 
         /// <summary>
-        /// Attempts to retrieve a cached <see cref="User"/> attached to the given <see cref="HttpContext"/>.
+        /// Attempts to retrieve a cached <see cref="User"/> attached to the given <see cref="HttpRequest"/>.
         /// If no users are cached, Hive will forward the request to the given <see cref="IProxyAuthenticationService"/>, and cache that result.
         /// </summary>
-        /// <param name="request">Context </param>
-        /// <param name="authenticationService"></param>
+        /// <param name="request">Request to retrieve/cache a <see cref="User"/> from.</param>
+        /// <param name="authenticationService">Authentication service to forward uncached requests to.</param>
         /// <returns>The <see cref="User"/> attached to this context, if any.</returns>
         // REVIEW: Would it be better to extend HttpContext or HttpRequest?
         public static async Task<User?> GetHiveUser(this HttpRequest request, IProxyAuthenticationService authenticationService)
