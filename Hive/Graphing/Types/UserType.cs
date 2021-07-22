3f67ffc413a478dc12192b58edfe403b44f6b378
Auros Nexus <aurosnex@gmail.com>
2021-07-22 03:27:35 -0400

    Removing mod-user searching GQL fields

--- Hive/Graphing/Types/UserType.cs
+++ Hive/Graphing/Types/UserType.cs
@@ -1,13 +1,7 @@
-﻿using Hive.Models;
-using GraphQL.Types;
+﻿using System;
 using System.Collections.Generic;
-using GraphQL;
-using Hive.Services.Common;
-using System;
-using Microsoft.AspNetCore.Http;
-using Hive.Services;
-using System.Linq;
-using System.Threading.Tasks;
+using GraphQL.Types;
+using Hive.Models;
 
 namespace Hive.Graphing.Types
 {
@@ -27,42 +21,8 @@ namespace Hive.Graphing.Types
             Name = nameof(User);
             Description = Resources.GraphQL.User;
 
-            _ = Field(u => u.Username)
-                .Description(Resources.GraphQL.User_Username);
-
-            _ = Field<ListGraphType<ModType>, IEnumerable<Mod>>()
-                .Name("uploaded").AddModFilters()
-                .Description(Resources.GraphQL.User_Uploaded)
-                .ResolveAsync((ctx) => GetAllModsFromUser(ctx, mod => mod.Uploader.Username == ctx.Source.Username));
-
-            _ = Field<ListGraphType<ModType>, IEnumerable<Mod>>()
-                .Name("contributedOn").AddModFilters()
-                .Description(Resources.GraphQL.User_ContributedOn)
-                .ResolveAsync((ctx) => GetAllModsFromUser(ctx, mod => mod.Contributors.Any(contributor => contributor.Username == ctx.Source.Username)));
-
-            _ = Field<ListGraphType<ModType>, IEnumerable<Mod>>()
-                .Name("authoredFor").AddModFilters()
-                .Description(Resources.GraphQL.User_AuthoredFor)
-                .ResolveAsync((ctx) => GetAllModsFromUser(ctx, mod => mod.Authors.Any(author => author.Username == ctx.Source.Username)));
-
             foreach (var graph in customGraphs)
                 graph.Configure(this);
-        }
-
-        private static async Task<IEnumerable<Mod>> GetAllModsFromUser(IResolveFieldContext<User> ctx, Func<Mod, bool> searchFunc)
-        {
-            (var modService, var http, var authService)
-                = ctx.RequestServices.GetRequiredServices<ModService, IHttpContextAccessor, IProxyAuthenticationService>();
-
-            var filterType = ctx.GetArgument("filter", ModType.Filter.Latest).ToString();
-            var channels = ctx.GetArgument<IEnumerable<string>?>("channelIds");
-            var gameVersion = ctx.GetArgument<string?>("gameVersion");
-
-            var user = await authService.GetUser(http.HttpContext!.Request).ConfigureAwait(false);
-            var queryResult = await modService.GetAllMods(user, channels?.ToArray(), gameVersion, filterType).ConfigureAwait(false);
-
-            ctx.Analyze(queryResult);
-            return queryResult.Value?.Where(searchFunc) ?? Array.Empty<Mod>();
         }
     }
 }
