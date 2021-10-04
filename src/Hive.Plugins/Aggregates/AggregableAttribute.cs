0c3e19502d74320c630695390ea0bcb0b919c9af
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-10-04 01:03:52 -0500

    Document new API surface.

--- src/Hive.Plugins/Aggregates/AggregableAttribute.cs
+++ src/Hive.Plugins/Aggregates/AggregableAttribute.cs
@@ -8,6 +8,9 @@ namespace Hive.Plugins.Aggregates
     [AttributeUsage(AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
     public sealed class AggregableAttribute : Attribute
     {
+        /// <summary>
+        /// Gets the type of the default implementation for this aggregate. This will be used if no services of the aggregated type are registered.
+        /// </summary>
         public Type? Default { get; set; }
     }
 }
