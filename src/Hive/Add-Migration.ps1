e95d8ca6d78cc22d02e7daea75082167df24527d
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-11-09 21:23:40 -0600

    Add helper script for adding migrations with non-standard intermediate output path

---
+++ src/Hive/Add-Migration.ps1
@@ -0,0 +1,13 @@
+﻿
+Push-Location "$PSScriptRoot"
+
+try {
+    $discard = dotnet tool restore
+
+    $baseIntermediateOutput = dotnet msbuild "$PSScriptRoot" -t:SdkPrintProperty -p:SdkPropertyToGet=BaseIntermediateOutputPath -v:m -nologo;
+    $baseIntermediateOutput = $baseIntermediateOutput.Trim();
+
+    dotnet ef migrations add -p "$PSScriptRoot" --msbuildprojectextensionspath "$baseIntermediateOutput" $args
+} finally {
+    Pop-Location
+}
---
+++ src/Hive/Bundle-Migrations.ps1
@@ -0,0 +1,13 @@
+﻿
+Push-Location "$PSScriptRoot"
+
+try {
+    $discard = dotnet tool restore
+
+    $baseIntermediateOutput = dotnet msbuild "$PSScriptRoot" -t:SdkPrintProperty -p:SdkPropertyToGet=BaseIntermediateOutputPath -v:m -nologo;
+    $baseIntermediateOutput = $baseIntermediateOutput.Trim();
+
+    dotnet ef migrations bundle -p "$PSScriptRoot" --msbuildprojectextensionspath "$baseIntermediateOutput" $args
+} finally {
+    Pop-Location
+}
