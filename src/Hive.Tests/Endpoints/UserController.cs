b6c2f6289d5ce81f06fa251bdd20f58741381cb3
Adam ? <adamznow@gmail.com>
2022-04-23 21:51:42 -0700

    Add test for getting self user info

--- src/Hive.Tests/Endpoints/UserController.cs
+++ src/Hive.Tests/Endpoints/UserController.cs
@@ -1,11 +1,18 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Threading.Tasks;
+using DryIoc;
+using Hive.Controllers;
 using Hive.Models;
 using Hive.Permissions;
 using Hive.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using NodaTime;
 using Xunit;
 using Xunit.Abstractions;
+using static Hive.Tests.TestHelpers;
 
 namespace Hive.Tests.Endpoints
 {
@@ -13,9 +20,39 @@ namespace Hive.Tests.Endpoints
     {
         private readonly ITestOutputHelper helper;
 
-        public UserController(ITestOutputHelper helper) : base(new PartialContext { Users = new[] { new User { Username = "asdf", AlternativeId = "1234" } } })
+        public UserController(ITestOutputHelper helper) : base(new PartialContext { Users = new[] { new User { Username = "test", AlternativeId = "1234" }, new User { Username = "test2", AlternativeId = "4321" } } })
         {
             this.helper = helper;
+        }
+
+        private IContainer CreateController(IEnumerable<IUserPlugin> plugins, string ruleRename, string ruleInfo)
+        {
+            var container = DIHelper.ConfigureServices(Options, _ => { }, helper, new UserControllerRuleProvider(ruleRename, ruleInfo));
+
+            container.RegisterInstance(plugins);
+            container.Register<Controllers.UserController>(Reuse.Scoped);
+
+            return container;
+        }
+
+        [Fact]
+        public async Task CanGetSelfUserInfo()
+        {
+            var serviceProvider = CreateController(new[] { new HiveUserPlugin() }, "next(true)", "next(true)");
+
+            using var scope = serviceProvider.CreateScope();
+            var controller = scope.ServiceProvider.GetRequiredService<Controllers.UserController>();
+            controller.ControllerContext.HttpContext = CreateMockRequest(new MemoryStream());
+            // Get mock request user info, which will have Bearer: asdf
+            // Should resolve to the test user.
+            var data = await controller.GetUserInfo(null);
+            Assert.NotNull(data);
+            var result = data.Result as OkObjectResult;
+            Assert.NotNull(result);
+            var value = result!.Value as Dictionary<string, object>;
+            Assert.NotNull(value);
+            Assert.True(value!.ContainsKey("username"));
+            Assert.Equal("test", value!["username"]);
         }
 
         private class UserControllerRuleProvider : IRuleProvider
